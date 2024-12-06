1e63fee6288834beaa5d77119476d382e04070ea
Isaacpenaforte <100225206+Isaacpenaforte@users.noreply.github.com>
2024-12-06 00:11:18 -0300

    adicionadas mensagens ao paciente após ser classificado com risco

--- PythonProject/relatorio_paciente.py
+++ PythonProject/relatorio_paciente.py
@@ -35,6 +35,11 @@ def fazer_predicao(dados_paciente, modelo, scaler):
 
     # Revela a predição ao usuário
     if predicao == 0:
-        print("\nO paciente não apresenta risco de doença cardíaca.")
+        print("\nParabéns! De acordo com nossa análise, o paciente não apresenta risco significativo de doença cardíaca no momento.")
     else:
-        print("\nO paciente apresenta risco de doença cardíaca.")
+        print("\nAtenção! Nossa análise indica que o paciente apresenta risco elevado de doença cardíaca.")
+        print("\nÉ fundamental que o paciente adote hábitos de vida mais saudáveis para controlar essas condições.")
+        print("\nProcure um médico para avaliação e acompanhamento em uma unidade de saúde mais proxíma de sua casa.")
+        print("\nTente substituir alimentos processados e fast food por por frutas, legumes, grãos integrais e carnes magras. Reduza o consumo de sal e alimentos industrializados.")
+        print("\nA prática regular de exercícios, como caminhar, nadar ou pedalar, pode ajudar a controlar o colesterol e a pressão arterial.")
+
